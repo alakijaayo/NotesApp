06451c27cc5e57f82dc1e294915a21f843bac359
NadiaAiraf <aidanfaria@gmail.com>
2018-10-15 16:58:55 +0100

    added ability to create and store notes

--- note-list-model-test.js
+++ note-list-model-test.js
@@ -12,6 +12,32 @@
     assert.isTrue(noteList.list.includes(note))
   }
 
+  function testReturnNotes() {
+    var noteList = new NotesList();
+    var note = new Notes('asdfasdf');
+    noteList.storeNote(note)
+    assert.isTrue(noteList.returnNotes().includes(note))
+  }
+
+  function testReturn2Notes() {
+    var noteList = new NotesList();
+    var note = new Notes('asdfasdf');
+    var note2 = new Notes('asdfasdf');
+    noteList.storeNote(note)
+    noteList.storeNote(note2)
+    assert.isTrue(noteList.returnNotes().includes(note))
+    assert.isTrue(noteList.returnNotes().includes(note2))
+  }
+
+  function testCreateAndStoreNote() {
+    var noteList = new NotesList();
+    noteList.createAndStoreNote('ayo');
+    assert.isTrue(noteList.list[0].note === 'ayo')
+  }
+
+  testCreateAndStoreNote();
+  testReturnNotes();
+  testReturn2Notes();
   testList();
   testStoreNote();
 })(this);
--- note-list-model.js
+++ note-list-model.js
@@ -6,5 +6,14 @@
   NotesList.prototype.storeNote = function (note) {
     this.list.push(note)
   }
+
+  NotesList.prototype.returnNotes = function () {
+    return this.list
+  };
+
+  NotesList.prototype.createAndStoreNote = function (text) {
+    this.list.push(new Notes(text))
+  };
+
   exports.NotesList = NotesList;
 })(this);
